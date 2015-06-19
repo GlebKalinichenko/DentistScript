17462ab91d868e43ad621f261d6db478296263c9
GlebKalinichenko <Glebjn@yandex.ua>
2015-06-19 16:16:38 +0300

    Select query for get value from countries

---
+++ SelectQuery/ChangeScript.php
@@ -0,0 +1,13 @@
+<?php
+	$mysqli = new mysqli("mysql.hostinger.ru", "u796014662_admin", "adminmyadmin", "u796014662_docs");
+    $data = array();
+	$result = $mysqli->query("SELECT * FROM Changes");
+    	if ($result->num_rows > 0) {
+ 	   // output data of each row
+    	while($row = $result->fetch_assoc()) {
+            $data[] = $row;
+    	}
+        echo json_encode($data);
+	}
+	
+?>
