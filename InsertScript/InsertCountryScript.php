e203840677a8fc6fd519d980a156c9efb02f0369
GlebKalinichenko <Glebjn@yandex.ua>
2015-06-19 19:10:46 +0300

    Get json

--- InsertScript/InsertCountryScript.php
+++ InsertScript/InsertCountryScript.php
@@ -1,4 +1,9 @@
 <?php
+	$response = array();
+	$josn = file_get_contents("php://input");
+	$josn_data = json_decode($josn, true);	
+	$country = $josn_data->{'country'};
+	echo $country;
 	$mysqli = new mysqli("mysql.hostinger.ru", "u796014662_admin", "adminmyadmin", "u796014662_docs");
 	$sql = "INSERT INTO Countries (Country) VALUES (Корея)";
 	$result = $mysqli->query($sql);
