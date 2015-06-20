b94541bd2387c1607f049727ad69f24642c7a394
GlebKalinichenko <Glebjn@yandex.ua>
2015-06-20 08:30:50 +0300

    Send country to mysql

--- InsertScript/InsertCountryScript.php
+++ InsertScript/InsertCountryScript.php
@@ -1,10 +1,20 @@
 <?php
-	$response = array();
+	/*$response = array();
 	$josn = file_get_contents("php://input");
 	$josn_data = json_decode($josn, true);	
 	$country = $josn_data->{'country'};
-	echo $country;
+	echo $country;*/
 	$mysqli = new mysqli("mysql.hostinger.ru", "u796014662_admin", "adminmyadmin", "u796014662_docs");
-	$sql = "INSERT INTO Countries (Country) VALUES (Корея)";
-	$result = $mysqli->query($sql);
+	/* проверка соединения */
+	if (mysqli_connect_errno()) {
+    	printf("Соединение не установлено: %s\n", mysqli_connect_error());
+    	exit();
+	}
+	$query = "INSERT INTO Countries (Country) VALUES ('Korea')";
+	$mysqli->query($query);
+	/*$query = "INSERT INTO myCity VALUES (NULL, 'Stuttgart', 'DEU', 'Stuttgart', 617000)";
+	$mysqli->query($query);*/
+
+	printf ("ID новой записи: %d.\n", $mysqli->insert_id);
+
 ?>
