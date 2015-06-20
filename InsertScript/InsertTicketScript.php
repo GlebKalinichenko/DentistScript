1af6d8e2d7e00a58eb18094bd64710efaf9dd8ff
GlebKalinichenko <Glebjn@yandex.ua>
2015-06-20 11:16:49 +0300

    Insert new record for table ticket

---
+++ InsertScript/InsertTicketScript.php
@@ -0,0 +1,20 @@
+<?php
+	$data = file_get_contents('php://input');
+	$json = json_decode($data);
+	$doctorKod = $json->{'doctorKod'};
+	$registrationKod = $json->{'registrationKod'};
+	$dateReception = $json->{'dateReception'};
+
+	$mysqli = new mysqli("mysql.hostinger.ru", "u796014662_admin", "adminmyadmin", "u796014662_docs");
+	/* проверка соединения */
+	if (mysqli_connect_errno()) {
+    	printf("Соединение не установлено: %s\n", mysqli_connect_error());
+    	exit();
+	}
+	$query = "INSERT INTO Tickets (DoctorKod, RegistrationKod, DateReception) VALUES ('$doctorKod', 
+		'$registrationKod', '$dateReception')";
+	$mysqli->query($query);
+
+	printf ("ID новой записи: %d.\n", $mysqli->insert_id);
+
+?>
