68073a7cc61ffe1fa8c3d058cd673a0d8f364eb2
GlebKalinichenko <Glebjn@yandex.ua>
2015-06-20 09:57:40 +0300

    Insert record about new country and insert record about new city

--- InsertScript/InsertCityScript.php
+++ InsertScript/InsertCityScript.php
@@ -1,17 +1,16 @@
 <?php
 	$data = file_get_contents('php://input');
 	$json = json_decode($data);
-	$service = $json->{'country'};
+	$countryKod = $json->{'countryKod'};
+	$city = $json->{'city'};
 	$mysqli = new mysqli("mysql.hostinger.ru", "u796014662_admin", "adminmyadmin", "u796014662_docs");
 	/* проверка соединения */
 	if (mysqli_connect_errno()) {
     	printf("Соединение не установлено: %s\n", mysqli_connect_error());
     	exit();
 	}
-	$query = "INSERT INTO Countries (Country) VALUES ('$service')";
+	$query = "INSERT INTO Cities (CountryKod, City) VALUES ('$countryKod', '$city')";
 	$mysqli->query($query);
-	/*$query = "INSERT INTO myCity VALUES (NULL, 'Stuttgart', 'DEU', 'Stuttgart', 617000)";
-	$mysqli->query($query);*/
 
 	printf ("ID новой записи: %d.\n", $mysqli->insert_id);
 
