60e4060f6cd07eb9ac2676b8fbb8ae6f741b7409
GlebKalinichenko <Glebjn@yandex.ua>
2015-06-20 08:38:48 +0300

    Query with params

--- InsertScript/InsertCountryScript.php
+++ InsertScript/InsertCountryScript.php
@@ -4,13 +4,14 @@
 	$josn_data = json_decode($josn, true);	
 	$country = $josn_data->{'country'};
 	echo $country;*/
+	$value = "Korea";
 	$mysqli = new mysqli("mysql.hostinger.ru", "u796014662_admin", "adminmyadmin", "u796014662_docs");
 	/* проверка соединения */
 	if (mysqli_connect_errno()) {
     	printf("Соединение не установлено: %s\n", mysqli_connect_error());
     	exit();
 	}
-	$query = "INSERT INTO Countries (Country) VALUES ('Korea')";
+	$query = "INSERT INTO Countries (Country) VALUES ('$value')";
 	$mysqli->query($query);
 	/*$query = "INSERT INTO myCity VALUES (NULL, 'Stuttgart', 'DEU', 'Stuttgart', 617000)";
 	$mysqli->query($query);*/
