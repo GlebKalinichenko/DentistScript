ef05d7e2fc2f9a97e727fb543f33b2fe56c34187
GlebKalinichenko <Glebjn@yandex.ua>
2015-06-22 18:48:33 +0300

    Update field Department in table DepartmentDoctors

---
+++ UpdateScript/DepartmetDoctors/UpdateDepartmentDoctorsScript.php
@@ -0,0 +1,17 @@
+<?php
+	$data = file_get_contents('php://input');
+	$json = json_decode($data);
+	$oldDepartment = $json->{'oldDepartment'};
+	$newDepartment = $json->{'newDepartment'};
+	$mysqli = new mysqli("mysql.hostinger.ru", "u796014662_admin", "adminmyadmin", "u796014662_docs");
+	/* проверка соединения */
+	if (mysqli_connect_errno()) {
+    	printf("Соединение не установлено: %s\n", mysqli_connect_error());
+    	exit();
+	}
+	$query = "UPDATE DepartmentDoctors SET Department='$newDepartment' WHERE Department='$oldDepartment'";
+	$mysqli->query($query);
+
+	printf ("ID новой записи: %d.\n", $mysqli->insert_id);
+
+?>
