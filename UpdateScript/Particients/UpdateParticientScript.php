11580fa647b56e2dab7a277294ef7e570a6a2e7d
GlebKalinichenko <Glebjn@yandex.ua>
2015-06-23 18:31:04 +0300

    Update fields CiytKod Address PhoneNumber DateBorn FioParent

--- UpdateScript/Particients/UpdateParticientScript.php
+++ UpdateScript/Particients/UpdateParticientScript.php
@@ -3,13 +3,26 @@
 	$json = json_decode($data);
 	$oldFio = $json->{'oldFio'};
 	$newFio = $json->{'newFio'};
+	$oldPhoneNumber = $json->{'oldPhoneNumber'};
+	$newPhoneNumber = $json->{'newPhoneNumber'};
+	$oldAddress = $json->{'oldAddress'};
+	$newAddress = $json->{'newAddress'};
+	$oldCityKod = $json->{'oldCityKod'};
+	$newCityKod = $json->{'newCityKod'};
+	$oldDateBorn = $json->{'oldDateBorn'};
+	$newDateBorn = $json->{'newDateBorn'};
+	$oldFIOParent = $json->{'oldFIOParent'};
+	$newFIOParent = $json->{'newFIOParent'};
 	$mysqli = new mysqli("mysql.hostinger.ru", "u796014662_admin", "adminmyadmin", "u796014662_docs");
 	/* проверка соединения */
 	if (mysqli_connect_errno()) {
     	printf("Соединение не установлено: %s\n", mysqli_connect_error());
     	exit();
 	}
-	$query = "UPDATE Particients SET FIO='$newFio' WHERE FIO='$oldFio'";
+	$query = "UPDATE Particients SET FIO = '$newFio', CityKod = '$newCityKod', 
+	PhoneNumber = '$newPhoneNumber', DateBorn = '$newDateBorn', FIOParent = '$newFIOParent' 
+	WHERE FIO='$oldFio', CityKod = '$oldCityKod', PhoneNumber = '$oldPhoneNumber', 
+	DateBorn = '$oldDateBorn', FIOParent = '$oldFIOParent";
 	$mysqli->query($query);
 
 	printf ("ID новой записи: %d.\n", $mysqli->insert_id);
