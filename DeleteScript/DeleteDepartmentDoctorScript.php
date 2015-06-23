c34e54a2db7383522e71bb44ab78fa0c56ce289b
GlebKalinichenko <Glebjn@yandex.ua>
2015-06-23 22:45:44 +0300

    Delete record from table DepartmentsDoctors

---
+++ DeleteScript/DeleteDepartmentDoctorScript.php
@@ -0,0 +1,16 @@
+<?php
+	$data = file_get_contents('php://input');
+	$json = json_decode($data);
+	$department = $json->{'department'};
+	$mysqli = new mysqli("mysql.hostinger.ru", "u796014662_admin", "adminmyadmin", "u796014662_docs");
+	/* проверка соединения */
+	if (mysqli_connect_errno()) {
+    	printf("Соединение не установлено: %s\n", mysqli_connect_error());
+    	exit();
+	}
+	$query = "DELETE FROM DepartmentsDoctors WHERE Department = '$department'";
+	$mysqli->query($query);
+
+	printf ("ID новой записи: %d.\n", $mysqli->insert_id);
+
+?>
