8ffc160ddb976f3fb3e046afdb48359eca50dfcb
GlebKalinichenko <Glebjn@yandex.ua>
2015-06-23 16:28:50 +0300

    Fields CountryKod City for update in table Cities

--- UpdateScript/Cities/UpdateCityInCities.php
+++ UpdateScript/Cities/UpdateCityInCities.php
@@ -1,6 +1,8 @@
 <?php
 	$data = file_get_contents('php://input');
 	$json = json_decode($data);
+	$oldCountryKod = $json->{'oldCountryKod'};
+	$newCountryKod = $json->{'newCountryKod'};
 	$oldCity = $json->{'oldCity'};
 	$newCity = $json->{'newCity'};
 	$mysqli = new mysqli("mysql.hostinger.ru", "u796014662_admin", "adminmyadmin", "u796014662_docs");
@@ -9,7 +11,7 @@
     	printf("Соединение не установлено: %s\n", mysqli_connect_error());
     	exit();
 	}
-	$query = "UPDATE Cities SET City='$newCity' WHERE City='$oldCity'";
+	$query = "UPDATE Cities SET CountryKod = '$newCountryKod', City='$newCity' WHERE CountryKod = 'oldCountryKod', City='$oldCity'";
 	$mysqli->query($query);
 
 	printf ("ID новой записи: %d.\n", $mysqli->insert_id);
