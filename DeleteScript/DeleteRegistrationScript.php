ee074b741e3de11be9cc4a72fa02b03a3930ede7
GlebKalinichenko <Glebjn@yandex.ua>
2015-06-24 18:01:42 +0300

    Delete record in table Registrations

---
+++ DeleteScript/DeleteRegistrationScript.php
@@ -0,0 +1,16 @@
+<?php
+	$data = file_get_contents('php://input');
+	$json = json_decode($data);
+	$registration = $json->{'IdRegistration'};
+	$mysqli = new mysqli("mysql.hostinger.ru", "u796014662_admin", "adminmyadmin", "u796014662_docs");
+	/* проверка соединения */
+	if (mysqli_connect_errno()) {
+    	printf("Соединение не установлено: %s\n", mysqli_connect_error());
+    	exit();
+	}
+	$query = "DELETE FROM Registrations WHERE IdRegistration = '$registration'";
+	$mysqli->query($query);
+
+	printf ("ID новой записи: %d.\n", $mysqli->insert_id);
+
+?>
