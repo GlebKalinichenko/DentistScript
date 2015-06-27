ee6ce025c3a1be642ba334b35fbd2dd06ba6c117
GlebKalinichenko <Glebjn@yandex.ua>
2015-06-27 18:04:46 +0300

    Select fields from table Cities with lookup field CountryKod

---
+++ SelectQueryLookup/SelectCityLookup.php
@@ -0,0 +1,13 @@
+<?php
+	$mysqli = new mysqli("mysql.hostinger.ru", "u796014662_admin", "adminmyadmin", "u796014662_docs");
+    $data = array();
+    $result = $mysqli->query("SELECT Countries.Country, Cities.City FROM Cities INNER JOIN Countries ON Cities.CountryKod = Countries.IdCountry");
+    	if ($result->num_rows > 0) {
+ 	   // output data of each row
+    	while($row = $result->fetch_assoc()) {
+            $data[] = $row;
+    	}
+        echo json_encode($data);
+	}
+	
+?>
